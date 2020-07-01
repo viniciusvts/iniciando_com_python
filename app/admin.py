596d4c296d18700e0cbd52b027abb4ee363f79d7
Vinicius de Santana <vinicius.vts@gmail.com>
2020-06-30 23:35:29 -0300

    add category , product and tag to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from app.models import Category, Product, Tag
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'descricao', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'is_active', 'get_tags']
+    list_filter = ['is_active']
+    search_fields = ['name']
+
+    def get_tags(self, obj):
+        return ", ".join([t.name for t in obj.tags.all()])
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Tag, TagAdmin)
